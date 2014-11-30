d40aba35b9665ea14bbc1502f3eafd2e0b270035
artemskiy <garcia82@yandex.ru>
2014-12-01 01:49:28 +0400

    Add Laravel 5 helpers.php

---
+++ app/lib/helpers5.php
@@ -0,0 +1,238 @@
+<?php
+
+if ( ! function_exists('abort'))
+{
+	/**
+	 * Throw an HttpException with the given data.
+	 *
+	 * @param  int     $code
+	 * @param  string  $message
+	 * @param  array   $headers
+	 * @return void
+	 *
+	 * @throws \Symfony\Component\HttpKernel\Exception\HttpException
+	 * @throws \Symfony\Component\HttpKernel\Exception\NotFoundHttpException
+	 */
+	function abort($code, $message = '', array $headers = array())
+	{
+		return app()->abort($code, $message, $headers);
+	}
+}
+
+if ( ! function_exists('bcrypt'))
+{
+	/**
+	 * Hash the given value.
+	 *
+	 * @param  string  $value
+	 * @param  array   $options
+	 * @return string
+	 */
+	function bcrypt($value, $options = array())
+	{
+		//return app('hash')->make($value, $options);
+	}
+}
+
+if ( ! function_exists('config'))
+{
+	/**
+	 * Get the specified configuration value.
+	 *
+	 * @param  string  $key
+	 * @param  mixed   $default
+	 * @return mixed
+	 */
+	function config($key, $default = null)
+	{
+		return app('config')->get($key, $default);
+	}
+}
+
+if ( ! function_exists('cookie'))
+{
+	/**
+	 * Create a new cookie instance.
+	 *
+	 * @param  string  $name
+	 * @param  string  $value
+	 * @param  int     $minutes
+	 * @param  string  $path
+	 * @param  string  $domain
+	 * @param  bool    $secure
+	 * @param  bool    $httpOnly
+	 * @return \Symfony\Component\HttpFoundation\Cookie
+	 */
+	function cookie($name = null, $value = null, $minutes = 0, $path = null, $domain = null, $secure = false, $httpOnly = true)
+	{
+		$cookie = app('cookie');
+
+		if (is_null($name))
+		{
+			return $cookie;
+		}
+
+		//return $cookie->make($name, $value, $minutes, $path, $domain, $secure, $httpOnly);
+	}
+}
+
+if ( ! function_exists('delete'))
+{
+	/**
+	 * Register a new DELETE route with the router.
+	 *
+	 * @param  string  $uri
+	 * @param  \Closure|array|string  $action
+	 * @return \Illuminate\Routing\Route
+	 */
+	function delete($uri, $action)
+	{
+		return app('router')->delete($uri, $action);
+	}
+}
+
+if ( ! function_exists('get'))
+{
+	/**
+	 * Register a new GET route with the router.
+	 *
+	 * @param  string  $uri
+	 * @param  \Closure|array|string  $action
+	 * @return \Illuminate\Routing\Route
+	 */
+	function get($uri, $action)
+	{
+		return app('router')->get($uri, $action);
+	}
+}
+
+if ( ! function_exists('info'))
+{
+	/**
+	 * Write some information to the log.
+	 *
+	 * @param  string  $message
+	 * @param  array   $context
+	 * @return void
+	 */
+	function info($message, $context = array())
+	{
+		return app('log')->info($message, $context);
+	}
+}
+
+if ( ! function_exists('logger'))
+{
+	/**
+	 * Log a debug message to the logs.
+	 *
+	 * @param  string  $message
+	 * @param  array  $context
+	 * @return void
+	 */
+	function logger($message, array $context = array())
+	{
+		return app('log')->debug($message, $context);
+	}
+}
+
+if ( ! function_exists('old'))
+{
+	/**
+	 * Retrieve an old input item.
+	 *
+	 * @param  string  $key
+	 * @param  mixed   $default
+	 * @return mixed
+	 */
+	function old($key = null, $default = null)
+	{
+		return app('request')->old($key, $default);
+	}
+}
+
+if ( ! function_exists('patch'))
+{
+	/**
+	 * Register a new PATCH route with the router.
+	 *
+	 * @param  string  $uri
+	 * @param  \Closure|array|string  $action
+	 * @return \Illuminate\Routing\Route
+	 */
+	function patch($uri, $action)
+	{
+		return app('router')->patch($uri, $action);
+	}
+}
+
+if ( ! function_exists('post'))
+{
+	/**
+	 * Register a new POST route with the router.
+	 *
+	 * @param  string  $uri
+	 * @param  \Closure|array|string  $action
+	 * @return \Illuminate\Routing\Route
+	 */
+	function post($uri, $action)
+	{
+		return app('router')->post($uri, $action);
+	}
+}
+
+if ( ! function_exists('put'))
+{
+	/**
+	 * Register a new PUT route with the router.
+	 *
+	 * @param  string  $uri
+	 * @param  \Closure|array|string  $action
+	 * @return \Illuminate\Routing\Route
+	 */
+	function put($uri, $action)
+	{
+		return app('router')->put($uri, $action);
+	}
+}
+
+if ( ! function_exists('redirect'))
+{
+	/**
+	 * Get an instance of the redirector.
+	 *
+	 * @param  string|null  $to
+	 * @param  int     $status
+	 * @param  array   $headers
+	 * @param  bool    $secure
+	 * @return \Illuminate\Routing\Redirector|\Illuminate\Http\RedirectResponse
+	 */
+	function redirect($to = null, $status = 302, $headers = array(), $secure = null)
+	{
+		if ( ! is_null($to))
+		{
+			return app('redirect')->to($to, $status, $headers, $secure);
+		}
+		else
+		{
+			return app('redirect');
+		}
+	}
+}
+
+if ( ! function_exists('response'))
+{
+	/**
+	 * Return a new response from the application.
+	 *
+	 * @param  string  $content
+	 * @param  int     $status
+	 * @param  array   $headers
+	 * @return \Symfony\Component\HttpFoundation\Response|\Illuminate\Contracts\Routing\ResponseFactory
+	 */
+	function response($content = '', $status = 200, array $headers = array())
+	{	
+		//return \Illuminate\Support\Facades\Response::make($content, $status, $headers);
+	}
+}
+
