5e3456131e6d185a0a04bb7aa7d4446265b18757
artemskiy <garcia82@yandex.ru>
2014-12-06 23:48:15 +0400

    User controller

--- app/controllers/ProductController.php
+++ app/controllers/ProductController.php
@@ -64,54 +64,68 @@ class ProductController extends \BaseController {
 
 		if(!is_object($product)) { return $this->show('new'); }
 		
-		$paginator_and_sorting = get_paginator_and_sorting();
-		
-			$sub = $vdb->productOnlySubProductsQuery($this->veer->siteId, $id, $paginator_and_sorting);
-
-			$parent = $vdb->productOnlyParentProductsQuery($this->veer->siteId, $id, $paginator_and_sorting);
-
-			$categories = $vdb->productOnlyCategoriesQuery($this->veer->siteId, $id, $paginator_and_sorting);
-
-			$pages = $vdb->productOnlyPagesQuery($this->veer->siteId, $id, $paginator_and_sorting);
-                
-		$product->increment('viewed');	
-
-		$product->load('images', 'tags', 'attributes', 'downloads', 'userlists');
-
-		// TODO: comments system
-		
-		// TODO: connected?=sub/parent new? ordered? viewed?
-		
-		// TODO: groups
-		
-		/*if($page->show_comments == 1) { 
-			if($comments_system == "disqus") { 
-
-				$data_comments['disqus_shortname'] = array_get(Config::get('veer.site_config'),'COMMENTS_DISQUS_ID'); 
-				$data_comments['disqus_identifier'] = '_page_' . $pid;
-				$data_comments['disqus_title'] = $p->title;
-				$data_comments['disqus_url'] = URL::full();                    
-				$path_to_comments_template = 'disqus';
-			} else {
-
-				$p->load('comments');
-
-				$data_comments = $p->comments->toArray();
-			}
-		}*/	
-		
-		$data = array(
-			"product" => $product,
-			"subproducts" => $sub,
-			"parentproducts" => $parent,
-			"pages" => $pages,
-			"categories" => $categories,
-			"data" => $this->veer->loadedComponents,
-			"template" => $this->template
-		); 
-					
-		$view = view($this->template.'.product', $data);
+		if($method != "sortingProducts") {
 			
+			$paginator_and_sorting = get_paginator_and_sorting();
+
+				$sub = $vdb->productOnlySubProductsQuery($this->veer->siteId, $id, $paginator_and_sorting);
+
+				$parent = $vdb->productOnlyParentProductsQuery($this->veer->siteId, $id, $paginator_and_sorting);
+
+				$categories = $vdb->productOnlyCategoriesQuery($this->veer->siteId, $id, $paginator_and_sorting);
+
+				$pages = $vdb->productOnlyPagesQuery($this->veer->siteId, $id, $paginator_and_sorting);
+
+			$product->increment('viewed');	
+
+			$product->load('images', 'tags', 'attributes', 'downloads', 'userlists');
+
+			// TODO: comments system
+
+			// TODO: connected?=sub/parent new? ordered? viewed?
+
+			// TODO: groups
+
+			/*if($page->show_comments == 1) { 
+				if($comments_system == "disqus") { 
+
+					$data_comments['disqus_shortname'] = array_get(Config::get('veer.site_config'),'COMMENTS_DISQUS_ID'); 
+					$data_comments['disqus_identifier'] = '_page_' . $pid;
+					$data_comments['disqus_title'] = $p->title;
+					$data_comments['disqus_url'] = URL::full();                    
+					$path_to_comments_template = 'disqus';
+				} else {
+
+					$p->load('comments');
+
+					$data_comments = $p->comments->toArray();
+				}
+			}*/	
+
+			$data = array(
+				"product" => $product,
+				"subproducts" => $sub,
+				"parentproducts" => $parent,
+				"pages" => $pages,
+				"categories" => $categories,
+				"data" => $this->veer->loadedComponents,
+				"template" => $this->template
+			); 
+			
+			$view = view($this->template.'.product', $data);
+
+		} else {
+			
+			$data = array(
+				"products" => $product,
+				"data" => $this->veer->loadedComponents,
+				"template" => $this->template
+			); 
+			
+			$view = view($this->template.'.products', $data);
+			
+		}
+		
 		$this->view = $view; 
 
 		return $view;	
