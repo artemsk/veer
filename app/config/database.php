3683a6a6c90ac68e08299ebc32818d1421c51734
artemskiy <garcia82@yandex.ru>
2014-12-12 18:32:47 +0400

    Database settings

--- app/config/database.php
+++ app/config/database.php
@@ -26,7 +26,7 @@ return array(
 	|
 	*/
 
-	'default' => 'mysql',
+	'default' => $_ENV['DBDRVR'],
 
 	/*
 	|--------------------------------------------------------------------------
@@ -48,7 +48,7 @@ return array(
 
 		'sqlite' => array(
 			'driver'   => 'sqlite',
-			'database' => __DIR__.'/../database/production.sqlite',
+			'database' => __DIR__.'/../database/' . $_ENV['DBNAME'], //
 			'prefix'   => '',
 		),
 
