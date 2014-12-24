0b47816db47a49292a78e6f33fb1f8ec0f8028f3
artemskiy <garcia82@yandex.ru>
2014-12-24 15:33:34 +0400

    E-commerce Forms

--- app/views/template-admin/discounts.blade.php
+++ app/views/template-admin/discounts.blade.php
@@ -1,16 +1,16 @@
 @extends($template.'.layout.base')
 
 @section('body')
-	<ol class="breadcrumb">
-		<li><strong>Users</strong></li>
-		<li><a href="{{ route("admin.show", "books") }}">Books</a></li>
-		<li><a href="{{ route("admin.show", "lists") }}">Lists</a></li>
-		<li><a href="{{ route("admin.show", "searches") }}">Searches</a></li>		
-		<li><a href="{{ route("admin.show", "comments") }}">Comments</a></li>	
-		<li><a href="{{ route("admin.show", "communications") }}">Communications</a></li>
-		<li><a href="{{ route("admin.show", "roles") }}">Roles</a></li>
-	</ol> 
-<h1>Users <small>sort by created | name</small></h1>
+<ol class="breadcrumb">
+		<li><strong>E-commerce</strong></li>
+		<li><a href="{{ route("admin.show", "orders") }}">Orders</a></li>
+		<li><a href="{{ route("admin.show", "bills") }}">Bills</a></li>
+		<li class="active">Discounts</li>
+		<li><a href="{{ route("admin.show", "shipping") }}">Shipping methods</a></li>		
+		<li><a href="{{ route("admin.show", "payment") }}">Payment methods</a></li>	
+		<li><a href="{{ route("admin.show", "statuses") }}">Statuses</a></li>
+</ol>
+<h1>Discounts</h1>
 <br/>
 <div class="container">
 	@foreach($items as $item)
@@ -19,6 +19,45 @@
 			
 	@endforeach
 	
-	
+	<div class="row">
+        <div class="col-md-6">             
+              <div class="form-group">
+                <label>Users ID</label>
+                <input type="text" class="form-control" name="InUsers" placeholder="Users ID">
+              </div>
+            <div class="form-group">
+                <label>Sites ID</label>
+                <input type="text" class="form-control" name="InSite" placeholder="Sites ID">
+              </div>
+             <div class="form-group">
+                <label>Secret Code</label>
+                <input type="text" class="form-control" name="InSecret" placeholder="Secret Code">
+            </div>
+            <div class="form-group">
+                <label>Status (active, wait, expired, canceled) </label>
+                <input type="text" class="form-control" name="InStatus" placeholder="Status">
+            </div>             
+              <button type="submit" class="btn btn-default">Submit</button>
+        </div>  
+        <div class="col-md-6">
+             <div class="form-group">
+                <label>Discount</label>
+                <input type="text" class="form-control" name="InDiscount" placeholder="Percent">
+            </div>
+            <div class="checkbox">
+                <label>
+                  <input type="checkbox" name="OnDiscountExp"> Expires?
+                </label>
+            </div>
+            <div class="form-group">
+                <label>Discount Expiration Day</label>
+                <input type="date" class="form-control" name="InDiscountExpDay" placeholder="YYYY-MM-DD">
+            </div> 
+             <div class="form-group">
+                <label>Discount Expiration Times</label>
+                <input type="date" class="form-control" name="InDiscountExpTimes" placeholder="Times">
+            </div> 
+        </div>
+    </div>
 </div>
 @stop
--- app/views/template-admin/shipping.blade.php
+++ app/views/template-admin/shipping.blade.php
@@ -1,16 +1,16 @@
 @extends($template.'.layout.base')
 
 @section('body')
-	<ol class="breadcrumb">
-		<li><strong>Users</strong></li>
-		<li><a href="{{ route("admin.show", "books") }}">Books</a></li>
-		<li><a href="{{ route("admin.show", "lists") }}">Lists</a></li>
-		<li><a href="{{ route("admin.show", "searches") }}">Searches</a></li>		
-		<li><a href="{{ route("admin.show", "comments") }}">Comments</a></li>	
-		<li><a href="{{ route("admin.show", "communications") }}">Communications</a></li>
-		<li><a href="{{ route("admin.show", "roles") }}">Roles</a></li>
-	</ol> 
-<h1>Users <small>sort by created | name</small></h1>
+<ol class="breadcrumb">
+		<li><strong>E-commerce</strong></li>
+		<li><a href="{{ route("admin.show", "orders") }}">Orders</a></li>
+		<li><a href="{{ route("admin.show", "bills") }}">Bills</a></li>
+		<li><a href="{{ route("admin.show", "discounts") }}">Discounts</a></li>
+		<li class="shipping">Shipping methods</li>		
+		<li><a href="{{ route("admin.show", "payment") }}">Payment methods</a></li>	
+		<li><a href="{{ route("admin.show", "statuses") }}">Statuses</a></li>
+</ol>
+<h1>Shipping methods</h1>
 <br/>
 <div class="container">
 	@foreach($items as $item)
@@ -19,6 +19,67 @@
 			
 	@endforeach
 	
+	
+	<div class="row">
+        @for ($i = 0; $i < 3; $i++) 
+        <div class="col-md-4">
+              <div class="form-group">
+                <label>Site ID</label>
+                <input type="text" class="form-control" name="InSite[{{ $i }}]" placeholder="Site ID">
+              </div>
+              <div class="form-group">
+                <label>Shipping Module Name</label>
+                <input type="text" class="form-control" name="InName[{{ $i }}]" placeholder="Name">
+              </div>
+              <div class="checkbox">
+                    <label>
+                      <input type="checkbox" name="OnEnable[{{ $i }}]" checked> ON / OFF
+                    </label>
+              </div>
+              <div class="form-group">
+                <label>Delivery Type (delivery, pickup, no-delivery)</label>
+                <input type="text" class="form-control" name="InDelivery[{{ $i }}]" placeholder="Type">
+              </div>
+              <div class="form-group">
+                <label>Payment Type (fix, calculator, free)</label>
+                <input type="text" class="form-control" name="InPayment[{{ $i }}]" placeholder="Type">
+              </div>
+              <div class="form-group">
+                <label>Price (if fix / or if failed calculation)</label>
+                <input type="text" class="form-control" name="InPrice[{{ $i }}]" placeholder="Price">
+              </div>
+              <div class="form-group">
+                <label>Discount (in %)</label>
+                <input type="text" class="form-control" name="InDiscount[{{ $i }}]" placeholder="Percent">
+              </div>
+              <div class="checkbox">
+                    <label>
+                      <input type="checkbox" name="OnDiscountEnable[{{ $i }}]"> Discount Enable
+                    </label>
+              </div>
+              <div class="form-group">
+                    <label>Discount Conditions</label>
+                    <textarea class="form-control" name="InDiscountConditions[{{ $i }}]" rows="1"></textarea>
+              </div>
+              <div class="form-group">
+                    <label>Address (if it's pickup and known addresses)</label>
+                    <textarea class="form-control" name="InAddress[{{ $i }}]" rows="1"></textarea>
+              </div>
+              <div class="form-group">
+                    <label>Other Options</label>
+                    <textarea class="form-control" name="InOther[{{ $i }}]" rows="1"></textarea>
+              </div>
+              <div class="form-group">
+                    <label>FUNCTION NAME</label>
+                    <input type="text" class="form-control" name="InFunc[{{ $i }}]" placeholder="Function">
+              </div>
+              <div class="form-group">
+                <label>Manual Order</label>
+                <input type="text" class="form-control" name="InOrder[{{ $i }}]" placeholder="Manual Order">
+              </div>
+        </div> 
+        @endfor
+</div><button type="submit" class="btn btn-default">Submit</button> 
 	
 </div>
 @stop
