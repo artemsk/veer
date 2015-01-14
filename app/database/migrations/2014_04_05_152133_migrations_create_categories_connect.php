baeb0bc303cd231076b913cd7d1823811aec1408
artemskiy <garcia82@yandex.ru>
2015-01-14 20:30:13 +0400

    Rename migrations

---
+++ app/database/migrations/2014_04_05_152133_migrations_create_categories_connect.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class MigrationsCreateCategoriesConnect extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('categories_connect', function($table) {
+                    $table->bigIncrements('id');
+                    $table->bigInteger('categories_id')->index();
+                    $table->bigInteger('elements_id')->index();
+                    $table->string('elements_type',255)->index();
+                });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('categories_connect');
+	}
+
+}
---
+++ app/database/migrations/2014_05_07_113659_migrations_add_text_urls.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class MigrationsAddTextUrls extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('products', function ($table) {
+                   $table->string('url',255)->after('id')->default('')->index();
+                });
+                
+		Schema::table('pages', function ($table) {
+                   $table->string('url',255)->after('id')->default('')->index();
+                });                
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('products', function ($table) {
+                   $table->dropColumn('url');
+                });
+                
+		Schema::table('pages', function ($table) {
+                   $table->dropColumn('url');
+                });
+	}
+
+}
