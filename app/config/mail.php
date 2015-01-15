1c65a1f9a8688d6bc07273fc7fd7c930c183ce43
artemskiy <garcia82@yandex.ru>
2015-01-16 02:32:25 +0400

    Email fix

--- app/config/mail.php
+++ app/config/mail.php
@@ -54,7 +54,7 @@ return array(
 	|
 	*/
 
-	'from' => array('address' => null, 'name' => null),
+	'from' => array('address' => 'noreplay@localhost', 'name' => null),
 
 	/*
 	|--------------------------------------------------------------------------
