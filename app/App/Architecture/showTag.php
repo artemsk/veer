23ed3365aa7a73329a78fe5f9920008594fc424e
artemskiy <garcia82@yandex.ru>
2015-02-01 03:22:44 +0400

    Refactoring (prepare for L5)

---
+++ app/App/Architecture/showTag.php
@@ -0,0 +1,17 @@
+<?php namespace Veer\Architecture;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of showTag
+ *
+ * @author Jerry
+ */
+class showTag {
+
+	
+	
+}
