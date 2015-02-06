84206aca60c8af905851b434a8014907cd90176c
artemskiy <garcia82@yandex.ru>
2015-02-06 16:51:04 +0400

    Move order

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,156 +2,98 @@
 
 use Veer\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Redirect;
+use Veer\Services\Show\Order as ShowOrder;
 
 class OrderController extends Controller {
 
 	protected $administrator = false;
 	
-	public function __construct()
+	protected $showOrder;
+	
+	public function __construct(ShowOrder $showOrder)
 	{
 		parent::__construct();		
+		
 		//$this->beforeFilter('auth', array('only' => array('index', 'show')));
+		
+		$this->showOrder = $showOrder;
 	}
-	
 	
 	/**
 	 * Display a listing of the resource.
-	 *
-	 * @return Response
 	 */
 	public function index()
 	{
-		$data = $this->veer->loadedComponents;            
-
-		$view = view($this->template.'.secret-order', array(
-			"data" => $data,
-			"template" => $data['template']
-		)); 
-
-		//$this->view = $view; // to cache
-
-		return $view;		
+		/* do not cache: $this->view = $view; */		
+		return view($this->template.'.secret-order', array(
+			"data" => $this->veer->loadedComponents,
+			"template" => $this->template
+		)); 	
 	}
-
-
-	/**
-	 * Show the form for creating a new resource.
-	 *
-	 * @return Response
-	 */
-	public function create() {}
-
-
+	
 	/**
 	 * Check secret code for order and get object if it exists.
-	 *
-	 * @return Response
 	 */
 	public function store()
 	{
 		if(\Input::has('password')) 
 		{                
 			$p = trim(\Input::get('password'));  
-			if( $p != '' ) 
+			if( !empty($p) ) 
 			{
 				$check = \Veer\Models\Order::whereHas('secrets', function($query) use ($p) {
 					$query->where('secret','=',$p);
 				})->pluck('id');
 				
-				if($check) {
-					$orders = app('veerdb')->make('order.show', $check, array('userId' => 0, 'administrator' => true));
-				
-					return $this->showOrder($orders);
+				if(!empty($check)) 
+				{
+					return $this->showingOrder( 
+						$this->showOrder->getOrderWithSite(app('veer')->siteId, $check, null, true)
+					);
 				}
 			}
 		}           
 		return $this->index(); 	
 	}
 
-	
 	/**
 	 * Display the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
 	 */
-	public function show($id, $force = false)
+	public function show($id)
 	{
 		$this->administrator = administrator();
 		
-		if(\Auth::check() || $this->administrator == true) {
+		if(\Auth::check() || $this->administrator == true) 
+		{	
+			return $this->showingOrder(
+				$this->showOrder->getOrderWithSite(app('veer')->siteId, $id, \Auth::id(), $this->administrator)
+			);	
+		}		
 			
-			$orders = app('veerdb')->route($id, array('userId' => \Auth::id(), 'administrator' => $this->administrator));
-			
-		} else {			
-			return $this->index(); 
-		}
-                
-		return $this->showOrder($orders);        
+		return $this->index();       
 	}
-
 	
 	/**
 	 * Display result.
-	 *
-	 * @param  int  $orders
-	 * @return Response
 	 */
-	protected function showOrder($orders) 
+	protected function showingOrder($orders) 
 	{		
 		if(!is_object($orders)) { return Redirect::route('index'); }
 		 
-		$orders->load('user', 'userbook', 'userdiscount', 'status', 'delivery', 'payment', 'status_history', 'products', 'bills', 'secrets');
+		$orders->load(
+			'user', 'userbook', 'userdiscount', 'status', 'delivery', 
+			'payment', 'status_history', 'products', 'bills', 'secrets',
+			'orderContent');
 				
+		return $this->viewIndex('order', $orders, false);
+		
 		// TODO: разбить на отдельные страницы
 		// TODO: вместе с products загружать images & downloads	[доступно после оплаты]	
 		// TODO: если просмотр по коду, то ничего делать нельзя без логина
-		
-		$data = $this->veer->loadedComponents;            
-
-		$view = view($this->template.'.order', array(
-			"order" => $orders,
-			"data" => $data,
-			"template" => $data['template']
-		)); 
-
-		//$this->view = $view; 
-
-		return $view;
 	}
 	
-	
-	/**
-	 * Show the form for editing the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function edit($id) {}
-
-
-	/**
-	 * Update the specified resource in storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function update($id) {}
-
-
-	/**
-	 * Remove the specified resource from storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function destroy($id) {}
-
-
 	/**
 	 * check Bills
-	 * @param type $lnk
-	 * @return type
 	 */
 	public function bills($id = null, $lnk = null) 
 	{
@@ -159,8 +101,8 @@ class OrderController extends Controller {
 		
 		if(\Auth::check() || $this->administrator == true) 
 		{	
-			return $this->showBill( 
-				app('veerdb')->route(array($id, $lnk), array('userId' => \Auth::id(), 'administrator' => $this->administrator))
+			return $this->showingBill( 
+				$this->showOrder->getBillWithSite(app('veer')->siteId, $id, $lnk, \Auth::id(), $this->administrator)
 			);
 		}		
 		
@@ -168,10 +110,9 @@ class OrderController extends Controller {
 	}
 	
 	/**
-	 * show Blls
-	 * @param type $bills
+	 * show Bills
 	 */
-	protected function showBill($bills)
+	protected function showingBill($bills)
 	{
 		if(!is_object($bills)) { return Redirect::route('index'); }
 		
@@ -187,7 +128,6 @@ class OrderController extends Controller {
 		return \Response::make($bills->content);
 	}
 	
-	
 	/**
 	 * Order was successful made
 	 */
@@ -195,25 +135,23 @@ class OrderController extends Controller {
 	{
 		if(\Session::has("successfulOrder"))
 		{		
-			$data = $this->veer->loadedComponents;            
-
-			$orders = app('veerdb')->make('order.show', \Session::get("successfulOrder"), array('userId' => \Auth::id(), 
-				'administrator' => administrator()));
+			$orders = $this->showOrder->getOrderWithSite(
+				app('veer')->siteId, \Session::get("successfulOrder"), \Auth::id(), administrator());
 
 			if(is_object($orders))
 			{
 				$orders->load('user', 'userbook', 'userdiscount', 'status', 'delivery', 
-					'payment', 'status_history', 'products', 'bills', 'secrets');
+					'payment', 'status_history', 'products', 'bills', 'secrets', 'orderContent');
+				
 				// TODO: do we need to load all information?
 				// TODO: downloads for digital products
 
-				$view = view($this->template.'.success-order', array(
+				/* do not cache */
+				return view($this->template.'.success-order', array(
 					"order" => $orders,
-					"data" => $data,
-					"template" => $data['template']
-				)); 
-
-				return $view;	
+					"data" => $this->veer->loadedComponents,
+					"template" => $this->template
+				)); 	
 			}
 		}
 		
