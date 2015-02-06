5584a02377ddc98227dc59d0d6cd0f377b9ae5a8
artemskiy <garcia82@yandex.ru>
2015-02-07 01:00:34 +0400

    Cleaning

--- resources/views/template-admin/users.blade.php
+++ resources/views/template-admin/users.blade.php
@@ -4,7 +4,7 @@
 
 	@include($template.'.layout.breadcrumb-user', array('place' => 'users'))
 
-<h1>Users :{{ array_pull($items, 'counted') }} <small>
+<h1>Users :{{ array_pull($data, 'counted') }} <small>
 	@if(Input::get('filter',null) != null) 
 	filtered by <strong>#{{ Input::get('filter',null) }}:{{ Input::get('filter_id',null) }}</strong> | 
 	@endif		
