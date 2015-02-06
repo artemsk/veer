1f4ef1d963de350ff8dc9fc58e4c3b8f3f1e3b99
artemskiy <garcia82@yandex.ru>
2015-02-06 19:27:01 +0400

    Move filter

--- app/Services/Show/Filter.php
+++ app/Services/Show/Filter.php
@@ -1,8 +1,7 @@
 <?php namespace Veer\Services\Show;
 
 class Filter {
-	//put your code here
-	
+
 	/**
 	 * Query Builder: 
 	 * 
@@ -10,85 +9,129 @@ class Filter {
 	 * - with: Images
 	 * - to whom: make() | filter/{id[0].id[1].id[2].id[3]}
 	 * 
-	 * @params: FILTER_ATTRS key|value 
+	 * TODO: FILTER_ATTRS key|value ?
 	 */
-	public function filterShowQuery($siteId, $id, $queryParams)
+	public function getFilter($siteId, $id, $queryParams = array())
 	{
-		$p = null;
-
-		$id = explode('.', $id);
+		$p = array('products' => array(), 'pages' => array());
+		
+		$id = explode('-', $id);
 
 		$category_id = $id[0] ? $id[0] : 0;
 
 		// If the value of first key of array is "null", we will change
 		// condition operator from "=" to ">" (0) to include all categories in our
 		// filter which exist for our site.               
-		$c = "=";
-		if ($category_id <= 0) {
-			$c = ">";
-		}
-
-		$a = array();
+		$c = "=";  if ($category_id <= 0) $c = ">";
 
 		// Next we will walk through $id array to collect all filtered
 		// model attributes; we will skip blank and 0 values, then we'll 
 		// add collected data to our new array variable which we'll use twice - 
 		// for products and for pages filtering.
-		if (count($id) > 1) {
-			foreach ($id as $k => $filter) {
-				if ($k <= 0 || $filter <= 0) {
-					continue;
-				} // skip category
-
-				$a[$k] = Attribute::find($filter)->toArray();
-			}
-		}
+		$a = $this->collectFilteredAttributes($id);
 
 		// Queries for products model. First, we check if category is filtered,
 		// then we're going through attributes filters. Then we collect
 		// images, checking hidden and future products, and limits.
-		$p['products'] = Product::whereHas('categories', function($q) use ($category_id, $siteId, $c) {
-				$q->where('categories_id', $c, $category_id)->where('sites_id', '=', $siteId);
-			})->where(function($q) use ($a) {
-				if (count($a) > 0) {
-					foreach ($a as $filter) {
-						$q->whereHas('attributes', function($query) use ($filter) {
-							$query->where('name', '=', $filter['name'])->where('val', '=', $filter['val']);
-						});
-					}
-				}
-			})->with(array('images' => function($query) {
-				$query->orderBy('id', 'desc')->take(1);
-			}))->checked()
-			->orderBy($queryParams['sort'], $queryParams['direction'])
-			->take($queryParams['take'])
-			->skip($queryParams['skip'])
-			->get();
+		$p['products'] = $this->collectEntities('\Veer\Models\Product', $category_id, $siteId, $c, $a, $queryParams);
 
 		// Queries for pages model. First, we check if category is filtered,
 		// then we're going through attributes filters. Then we collect
 		// images, checking hidden, and limits.               
-		$p['pages'] = Page::whereHas('categories', function($q) use ($category_id, $siteId, $c) {
-				$q->where('categories_id', $c, $category_id)->where('sites_id', '=', $siteId);
-			})->where(function($q) use ($a) {
-				if (count($a) > 0) {
-					foreach ($a as $filter) {
+		$p['pages'] = $this->collectEntities('\Veer\Models\Page', $category_id, $siteId, $c, $a, $queryParams);
+
+		//
+		return $p;
+	}
+	
+	/* collect attributes */
+	protected function collectFilteredAttributes($id)
+	{
+		$a = array();
+		
+		if (count($id) > 1) 
+		{
+			foreach ($id as $k => $filter) 
+			{
+				if ($k <= 0 || $filter <= 0) continue; // skip category
+					
+				$a[$k] = Attribute::find($filter)->toArray();
+			}
+		}
+		
+		return $a;
+	}
+	
+	protected function collectEntities($model, $category_id, $siteId, $condition, $attributes, $queryParams = array())
+	{
+		$entity = $model::whereHas('categories', function($q) use ($category_id, $siteId, $condition) {
+				$q->where('categories_id', $condition, $category_id)->where('sites_id', '=', $siteId);
+			});
+	
+		$this->collectWithAttributes($entity, $attributes);
+	
+		$entity->with(array('images' => function($query) { $query->orderBy('id', 'desc')->take(1); }));
+	
+		if($model == '\Veer\Models\Product') $entity->checked();
+		
+		else $entity->excludeHidden();
+		
+		return $entity->orderBy(array_get($queryParams, 'sort', 'created_at'), array_get($queryParams, 'direction', 'desc'))
+			->take(array_get($queryParams, 'take', 25))
+			->skip(array_get($queryParams, 'skip', 0))->get();
+	}
+	
+	protected function collectWithAttributes($entity, $attributes)
+	{
+		return $entity->where(function($q) use ($attributes) {
+				if (count($attributes) > 0) {
+					foreach ($attributes as $filter) {
 						$q->whereHas('attributes', function($query) use ($filter) {
 							$query->where('name', '=', $filter['name'])->where('val', '=', $filter['val']);
 						});
 					}
 				}
-			})->with(array('images' => function($query) {
-				$query->orderBy('id', 'desc')->take(1);
-			}
-			))->excludeHidden()
-			->orderBy('created_at', 'desc')
-			->take($queryParams['take_pages'])
-			->skip($queryParams['skip_pages'])
-			->get();
-
-		//
-		return $p;
+		});
+	}
+	
+	protected function withModels($model, $products = array(), $pages = array())
+	{
+		$items = $model::select();
+		
+		if(count($products) > 0) {
+			$items->whereHas('products', function($query) use($products) {
+				$query->checked()->whereIn('products.id', $products->modelKeys());
+			});
+		}
+		
+		if(count($pages) > 0) {
+			$items->orWhereHas('pages', function($query) use($pages) {
+				$query->excludeHidden()->whereIn('pages.id', $pages->modelKeys());
+			});
+		}
+			
+		return app('veer')->cachingQueries->makeAndRemember($items, 'get', 5, null, 
+			md5($this->getFilterCacheName($model, $products, $pages)));
+	}
+	
+	public function withTags($products, $pages)
+	{
+		return $this->withModels('\Veer\Models\Tag', $products, $pages);
+	}
+	
+	public function withAttributes($products, $pages)
+	{
+		return $this->withModels('\Veer\Models\Attribute', $products, $pages);
+	}
+	
+	public function withCategories($products, $pages)
+	{
+		return $this->withModels('\Veer\Models\Category', $products, $pages);
+	}	
+	
+	protected function getFilterCacheName($model, $products, $pages)
+	{
+		return ($model."|".implode("|", $products->modelKeys())."|".implode("|", $pages->modelKeys()));
 	}
 	
 }
