7b163b4d261032fe86d275bb9d2a0f505f6ca190
artemskiy <garcia82@yandex.ru>
2015-02-24 19:13:03 +0400

    Tweak

--- app/Services/TemporaryTrait.php
+++ app/Services/TemporaryTrait.php
@@ -1,85 +1,93 @@
-<?php namespace Veer\Services;
+<?php
 
-trait TemporaryTrait {
-	
-	/**
-	 * Sending mails queue
-	 * 
-	 */
-	protected function message2mail($object, $emails = null, $recipients = null, $type = "communication")
-	{		
-		if(empty($emails) && empty($recipients)) return false;
-		
-		$place = $link = $subject = null;
-		
-		if($type == "communication") {
-			$siteUrl = $object->site->url;
-			
-			if(!empty($object->theme)) 
-			{ 
-				$subject = \Lang::get('veeradmin.emails.communication.subjectTheme', array(
-					'url' => $siteUrl, 'theme' => $object->theme
-				)); 
-			}
-			else {	$subject = \Lang::get('veeradmin.emails.communication.subject', array('url' => $siteUrl)); }
-		}
-		
-		if($type == "comment") {
-			$siteUrl = app('veer')->siteUrl;
-			$subject = \Lang::get('veeradmin.emails.comment.subject', array('url' => $siteUrl));
-		}
-		
-		switch ($object->elements_type) 
-		{
-			case "Veer\Models\Product":
-				$place = isset($object->elements->title) ? $object->elements->title : null;
-				$link = $siteUrl . "/product/" . $object->elements_id;
-			break;
+namespace Veer\Services;
 
-			case "Veer\Models\Page":
-				$place = isset($object->elements->title) ? $object->elements->title : null;
-				$link = $siteUrl . "/page/" . $object->elements_id;
-			break;
+trait TemporaryTrait
+{
 
-			case "Veer\Models\Category":
-				$place = isset($object->elements->title) ? $object->elements->title : null;
-				$link = $siteUrl . "/category/" . $object->elements_id;
-			break;
+    /**
+     * Sending mails queue
+     * Move
+     */
+    protected function message2mail($object, $emails = null, $recipients = null,
+                                    $type = "communication")
+    {
+        if (empty($emails) && empty($recipients)) return false;
 
-			case "Veer\Models\Order":
-				if(is_object($object->elements)) { $place = "#" . 
-					app('veershop')->getOrderId($object->elements->cluster, $object->elements->cluster_oid); }
-				$link = $siteUrl . "/user/";
-			break;
+        $place   = $link    = $subject = null;
 
-			default:
-				$place = $link = $object->url;
-			break;
-		}
+        if ($type == "communication") {
+            $siteUrl = $object->site->url;
 
-		if(empty($place) && !empty($link)) $place = $link;
-		
-		$data = array(
-			"sender" => isset($object->author) ? $object->author : $object->sender,
-			"txt" => isset($object->txt) ? $object->txt : $object->message,
-			"place" => $place,
-			"link" => $link
-		);
-		
-		if(is_array($recipients))
-		{
-			foreach($recipients as $userId)
-			{
-				$email = \Veer\Models\User::where('id','=',$userId)->pluck('email');
-				if(!empty($email)) $emails[] = $email;
-			}
-		}
-		
-		if(is_array($emails))
-		{
-			(new \Veer\Commands\SendEmailCommand( 'emails.'.str_plural($type), 
-				$data, $subject, array_unique($emails), null, $object->sites_id))->handle();
-		}
-	}
-	
+            if (!empty($object->theme)) {
+                $subject = \Lang::get('veeradmin.emails.communication.subjectTheme',
+                        array(
+                        'url' => $siteUrl, 'theme' => $object->theme
+                ));
+            } else {
+                $subject = \Lang::get('veeradmin.emails.communication.subject',
+                        array('url' => $siteUrl));
+            }
+        }
+
+        if ($type == "comment") {
+            $siteUrl = app('veer')->siteUrl;
+            $subject = \Lang::get('veeradmin.emails.comment.subject',
+                    array('url' => $siteUrl));
+        }
+
+        switch ($object->elements_type) {
+            case "Veer\Models\Product":
+                $place = isset($object->elements->title) ? $object->elements->title
+                        : null;
+                $link  = $siteUrl."/product/".$object->elements_id;
+                break;
+
+            case "Veer\Models\Page":
+                $place = isset($object->elements->title) ? $object->elements->title
+                        : null;
+                $link  = $siteUrl."/page/".$object->elements_id;
+                break;
+
+            case "Veer\Models\Category":
+                $place = isset($object->elements->title) ? $object->elements->title
+                        : null;
+                $link  = $siteUrl."/category/".$object->elements_id;
+                break;
+
+            case "Veer\Models\Order":
+                if (is_object($object->elements)) {
+                    $place = "#".
+                        app('veershop')->getOrderId($object->elements->cluster,
+                            $object->elements->cluster_oid);
+                }
+                $link = $siteUrl."/user/";
+                break;
+
+            default:
+                $place = $link  = $object->url;
+                break;
+        }
+
+        if (empty($place) && !empty($link)) $place = $link;
+
+        $data = array(
+            "sender" => isset($object->author) ? $object->author : $object->sender,
+            "txt" => isset($object->txt) ? $object->txt : $object->message,
+            "place" => $place,
+            "link" => $link
+        );
+
+        if (is_array($recipients)) {
+            foreach ($recipients as $userId) {
+                $email    = \Veer\Models\User::where('id', '=', $userId)->pluck('email');
+                if (!empty($email)) $emails[] = $email;
+            }
+        }
+
+        if (is_array($emails)) {
+            (new \Veer\Commands\SendEmailCommand('emails.'.str_plural($type),
+            $data, $subject, array_unique($emails), null, $object->sites_id))->handle();
+        }
+    }
 }
--- app/Services/VeerApp.php
+++ app/Services/VeerApp.php
@@ -1,320 +1,332 @@
-<?php namespace Veer\Services;
-	
+<?php
+
+namespace Veer\Services;
+
 use Illuminate\Support\Facades\Config;
 use Veer\Models\Component;
 
-class VeerApp {
+class VeerApp
+{
+    /**
+     *  Veer Layer.
+     *
+     */
+    const VEERVERSION = 'v1.1.6';
 
-	/**
-	 *  Veer Layer.
-	 * 
-	 */
-	const VEERVERSION = 'v1.1.6';
+    /**
+     * Veer Core Url.
+     * 
+     */
+    const VEERCOREURL = 'https://api.github.com/repos/artemsk/veer';
 
-	/** 
-	 * Veer Core Url.
-	 * 
-	 */
-	const VEERCOREURL = 'https://api.github.com/repos/artemsk/veer';
-	
-	/**
-	 *  Booted?
-	 * 
-	 */	
-	public $booted = false;
-	
-	/**
-	 *  Current url. 
-	 * 
-	 */	
-	public $siteId;	
+    /**
+     *  Booted?
+     *
+     */
+    public $booted = false;
 
-	/**
-	 *  Site Id associated with current url.
-	 * 
-	 */	
-	public $siteUrl;	
+    /**
+     *  Current url. 
+     * 
+     */
+    public $siteId;
 
-	/**
-	 *  Database dynamic configuration.
-	 * 
-	 */	
-	public $siteConfig = array();	
+    /**
+     *  Site Id associated with current url.
+     *
+     */
+    public $siteUrl;
 
-	/**
-	 *  Statistics.
-	 * 
-	 */		
-	public $statistics;
-	
-	/**
-	 *  Loaded components for current route.
-	 * 
-	 */		
-	public $loadedComponents;	
-	
-	/**
-	 *  Template variable.
-	 */
-	public $template;
-	
-	/**
-	 *  Work only with site-specific entities.
-	 */
-	public $isBoundSite = true;
-		
-	/**
-	 * Ajax return boolean
-	 */
-	public $forceEarlyResponse = false;
-	
-	/**
-	 * Early View container
-	 */
-	public $earlyResponseContainer;
-	
-	/**
-	 *  Cached Queries.
-	 */
-	public $cachingQueries;
-	
-	/**
-	 * Construct the VeerApp.
-	 *
-	 * 
-	 * 
-	 */
-	public function __construct()
-	{
-		$this->cachingQueries = new CachingQueries;
-	}
+    /**
+     *  Database dynamic configuration.
+     * 
+     */
+    public $siteConfig = array();
 
-	/**
-	 * is Site Filtered?
-	 *
-	 * 
-	 * 
-	 */
-	public function isBoundSite() 
-	{
-		return $this->isBoundSite;
-	}
-	
-	/**
-	 * Boot the VeerApp.
-	 *
-	 *
-	 * 
-	 */
-	public function run()
-	{		
-		\DB::enableQueryLog(); // TODO: remove
-		
-		$this->booted = true;
+    /**
+     *  Statistics.
+     *
+     */
+    public $statistics;
 
-		$this->siteUrl = $this->siteUrl();
-		
-		$siteDb = $this->isSiteAvailable($this->siteUrl);
+    /**
+     *  Loaded components for current route.
+     * 
+     */
+    public $loadedComponents;
 
-		$this->saveConfiguration($siteDb);	
-	}
-		
-	/**
-	 * Get Site Url with some cleaning. 
-	 * Mirrors/sites should be on the same level as Veer directory.
-	 *
-	 * 
-	 * @return string $url
-	 */
-	protected function siteUrl()
-	{ 		
-		$url = strtr(url(), array(
-				"www." => "",
-				"index.php/" => "",
-				"index.php" => "",
-		));
+    /**
+     *  Template variable.
+     */
+    public $template;
 
-		if (ends_with($url, "/")) {
-			$url = substr($url, 0, -1);
-		}
-		
-		return $url;
-	}
+    /**
+     *  Work only with site-specific entities.
+     */
+    public $isBoundSite = true;
 
-	/**
-	 * Check if this site's url exists in database & return its id.
-	 * Because of caching turning on/off and other changes
-	 * come to effect after cleaning cache only.
-	 *
-	 * @param string $siteUrl
-	 * @return \Veer\Models\Site $siteDb
-	 */
-	protected function isSiteAvailable($siteUrl)
-	{
-		$this->cachingQueries->make(\Veer\Models\Site::where('url', '=', $siteUrl)->where('on_off', '=', '1'));
-		
-		$siteDb = $this->cachingQueries->rememberForever('firstOrFail');
+    /**
+     * Ajax return boolean
+     */
+    public $forceEarlyResponse = false;
 
-		if ($siteDb->redirect_on == 1 && $siteDb->redirect_url != "") {
+    /**
+     * Early View container
+     */
+    public $earlyResponseContainer;
 
-			$siteDb = (new CachingQueries(\Veer\Models\Site::where('url', '=', $siteDb->redirect_url)
-					->where('on_off', '=', '1')))->rememberForever('firstOrFail');
-		}
+    /**
+     *  Cached Queries.
+     */
+    public $cachingQueries;
 
-		return $siteDb;
-	}
+    /**
+     * Construct the VeerApp.
+     *
+     * 
+     * 
+     */
+    public function __construct()
+    {
+        $this->cachingQueries = new CachingQueries;
+    }
 
-	/**
-	 * Loading site's configuration from database.
-	 *
-	 * 
-	 *
-	 */
-	protected function saveConfiguration($siteDb)
-	{
-		Config::set('veer.mainurl', $siteDb->url);
-		Config::set('veer.site_id', $siteDb->id);
+    /**
+     * is Site Filtered?
+     *
+     * 
+     * 
+     */
+    public function isBoundSite()
+    {
+        return $this->isBoundSite;
+    }
 
-		$this->siteId = $siteDb->id;
+    /**
+     * Boot the VeerApp.
+     *
+     *
+     * 
+     */
+    public function run()
+    {
+        \DB::enableQueryLog(); // TODO: remove
 
-		$this->cachingQueries->make(\Veer\Models\Configuration::where('sites_id', '=', $siteDb->id));
-		
-		$this->siteConfig = $this->cachingQueries->lists('conf_val', 'conf_key', 1440);
-	}
+        $this->booted = true;
 
-	/**
-	 * Load route components:
-	 * methods (immediate actions), events, queues etc.
-	 * 
-	 * 
-	 */
-	public function routePrepare($routeName)
-	{
-		$this->loadedComponents['template'] = $this->template =  
-			array_get($this->siteConfig, 'TEMPLATE', config('veer.template'));
-				
-		$this->registerComponents($routeName);
+        $this->siteUrl = $this->siteUrl();
 
-		$this->statistics();
-	}
-	
-	/**
-	 * Register components & events based on current route name & site. It allows
-	 * us to have different components and actions for different routes [and events 
-	 * on different sites].
-	 * 
-	 * 
-	 */
-	public function registerComponents($routeName, $params = null)
-	{
-		$this->cachingQueries->make(Component::validComponents($this->siteId, $routeName));
-		
-		$c = $this->cachingQueries->remember(1, 'get'); 
-				
-		foreach ($c as $component) 
-		{	
-			 $this->{camel_case('register '.$component->components_type)}($component->components_src, $params);
-		}
-	}
+        $siteDb = $this->isSiteAvailable($this->siteUrl);
 
-	/**
-	 * Register Functions.
-	 * 
-	 * @param string $src
-	 * @param mixed $params
-	 */
-	protected function registerFunctions($src, $params)
-	{
-		$this->loadedComponents['function'][$src] = $this->loadComponentClass($src, $params);
-	}
-	
-	/**
-	 * Register Events.
-	 * 
-	 * @param string $src
-	 * @param mixed $params
-	 */
-	protected function registerEvents($src, $params)
-	{
-		$this->loadComponentClass($src, $params, 'events');
-		
-		if(class_exists("\Veer\Events\\" . $src, false)) { 
-			\Illuminate\Support\Facades\Event::subscribe("\Veer\Events\\" . $src); 
-			
-			$this->loadedComponents['event'][$src] = true;
-			// now you can fire these events in templates etc.
-		}	
-	}
-	
-	/**
-	 * Register Pages
-	 * 
-	 * 
-	 * @param string $src
-	 */
-	protected function registerPages($src)
-	{
-		$this->loadedComponents['page'][$src] = \Veer\Models\Page::find($src); 
-	}
-	
-	/**
-	 * Loading custom classes for components, event subscribers.
-	 * 
-	 * 
-	 */
-	protected function loadComponentClass($className, $params = null, $type = "components")
-	{ 
-		/* Another vendor's component */
-		if (starts_with($className, '\\')) return $this->instantiateClass($className, $params, $type);
-		
-		$classFullName = "\Veer\\" . ucfirst($type) . "\\" . $className;
-		
-		if (!class_exists($classFullName)) $this->loadClassFromPath($className, $type);
-			
-		return $this->instantiateClass($classFullName, $params, $type);
-	}
+        $this->saveConfiguration($siteDb);
+    }
 
-	/**
-	 * Get path for components and require them.
-	 * 
-	 * 
-	 */
-	protected function loadClassFromPath($className, $type)
-	{
-		$pathComponent = base_path() . "/". config("veer." . $type . "_path") . "/" . $className . ".php";
-	
-		if (file_exists($pathComponent)) { require $pathComponent; } 
-	}
-	
-	/**
-	 * Instantiate class.
-	 * 
-	 * 
-	 */
-	protected function instantiateClass($classFullName, $params = null, $type = null)
-	{
-		if (class_exists($classFullName, false) && $type == "components") 
-		{
-			return new $classFullName($params);
-		}
-	}
-	
-	/**
-	 * Collecting statistics.
-	 *
-	 * 
-	 */
-	public function statistics()
-	{ 
-		$this->statistics['queries'] = count(\Illuminate\Support\Facades\DB::getQueryLog());
+    /**
+     * Get Site Url with some cleaning. 
+     * Mirrors/sites should be on the same level as Veer directory.
+     *
+     * 
+     * @return string $url
+     */
+    protected function siteUrl()
+    {
+        $url = strtr(url(),
+            array(
+            "www." => "",
+            "index.php/" => "",
+            "index.php" => "",
+        ));
 
-		$this->statistics['loading'] = round(microtime(true) - LARAVEL_START, 4);
+        if (ends_with($url, "/")) {
+            $url = substr($url, 0, -1);
+        }
 
-		$this->statistics['memory'] = number_format(memory_get_usage());
-		
-		$this->statistics['version'] = self::VEERVERSION;
+        return $url;
+    }
 
-		return $this->statistics;		
-	}	
-	
+    /**
+     * Check if this site's url exists in database & return its id.
+     * Because of caching turning on/off and other changes
+     * come to effect after cleaning cache only.
+     *
+     * @param string $siteUrl
+     * @return \Veer\Models\Site $siteDb
+     */
+    protected function isSiteAvailable($siteUrl)
+    {
+        $this->cachingQueries->make(\Veer\Models\Site::where('url', '=',
+                $siteUrl)->where('on_off', '=', '1'));
+
+        $siteDb = $this->cachingQueries->rememberForever('firstOrFail');
+
+        if ($siteDb->redirect_on == 1 && $siteDb->redirect_url != "") {
+            $siteDb = (new CachingQueries(\Veer\Models\Site::where('url', '=',
+                        $siteDb->redirect_url)
+                    ->where('on_off', '=', '1')))->rememberForever('firstOrFail');
+        }
+
+        return $siteDb;
+    }
+
+    /**
+     * Loading site's configuration from database.
+     *
+     *
+     *
+     */
+    protected function saveConfiguration($siteDb)
+    {
+        Config::set('veer.mainurl', $siteDb->url);
+        Config::set('veer.site_id', $siteDb->id);
+
+        $this->siteId = $siteDb->id;
+
+        $this->cachingQueries->make(\Veer\Models\Configuration::where('sites_id',
+                '=', $siteDb->id));
+
+        $this->siteConfig = $this->cachingQueries->lists('conf_val', 'conf_key',
+            1440);
+    }
+
+    /**
+     * Load route components:
+     * methods (immediate actions), events, queues etc.
+     *
+     *
+     */
+    public function routePrepare($routeName)
+    {
+        $this->loadedComponents['template'] = $this->template                     = array_get($this->siteConfig,
+            'TEMPLATE', config('veer.template'));
+
+        $this->registerComponents($routeName);
+
+        $this->statistics();
+    }
+
+    /**
+     * Register components & events based on current route name & site. It allows
+     * us to have different components and actions for different routes [and events 
+     * on different sites].
+     * 
+     * 
+     */
+    public function registerComponents($routeName, $params = null)
+    {
+        $this->cachingQueries->make(Component::validComponents($this->siteId,
+                $routeName));
+
+        $c = $this->cachingQueries->remember(1, 'get');
+
+        foreach ($c as $component) {
+            $this->{camel_case('register '.$component->components_type)}($component->components_src,
+                $params);
+        }
+    }
+
+    /**
+     * Register Functions.
+     *
+     * @param string $src
+     * @param mixed $params
+     */
+    protected function registerFunctions($src, $params)
+    {
+        $this->loadedComponents['function'][$src] = $this->loadComponentClass($src,
+            $params);
+    }
+
+    /**
+     * Register Events.
+     * 
+     * @param string $src
+     * @param mixed $params
+     */
+    protected function registerEvents($src, $params)
+    {
+        $this->loadComponentClass($src, $params, 'events');
+
+        if (class_exists("\Veer\Events\\".$src, false)) {
+            \Illuminate\Support\Facades\Event::subscribe("\Veer\Events\\".$src);
+
+            $this->loadedComponents['event'][$src] = true;
+            // now you can fire these events in templates etc.
+        }
+    }
+
+    /**
+     * Register Pages
+     * 
+     * 
+     * @param string $src
+     */
+    protected function registerPages($src)
+    {
+        $this->loadedComponents['page'][$src] = \Veer\Models\Page::find($src);
+    }
+
+    /**
+     * Loading custom classes for components, event subscribers.
+     * 
+     * 
+     */
+    protected function loadComponentClass($className, $params = null,
+                                          $type = "components")
+    {
+        /* Another vendor's component */
+        if (starts_with($className, '\\'))
+                return $this->instantiateClass($className, $params, $type);
+
+        $classFullName = "\Veer\\".ucfirst($type)."\\".$className;
+
+        if (!class_exists($classFullName))
+                $this->loadClassFromPath($className, $type);
+
+        return $this->instantiateClass($classFullName, $params, $type);
+    }
+
+    /**
+     * Get path for components and require them.
+     *
+     *
+     */
+    protected function loadClassFromPath($className, $type)
+    {
+        $pathComponent = base_path()."/".config("veer.".$type."_path")."/".$className.".php";
+
+        if (file_exists($pathComponent)) {
+            require $pathComponent;
+        }
+    }
+
+    /**
+     * Instantiate class.
+     * 
+     * 
+     */
+    protected function instantiateClass($classFullName, $params = null,
+                                        $type = null)
+    {
+        if (class_exists($classFullName, false) && $type == "components") {
+            return new $classFullName($params);
+        }
+    }
+
+    /**
+     * Collecting statistics.
+     *
+     * 
+     */
+    public function statistics()
+    {
+        $this->statistics['queries'] = count(\Illuminate\Support\Facades\DB::getQueryLog());
+
+        $this->statistics['loading'] = round(microtime(true) - LARAVEL_START, 4);
+
+        $this->statistics['memory'] = number_format(memory_get_usage());
+
+        $this->statistics['version'] = self::VEERVERSION;
+
+        return $this->statistics;
+    }
 }
