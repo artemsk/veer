15943b413d360e8d5ec3037a111031cf2e53e3ad
artemskiy <garcia82@yandex.ru>
2015-02-28 19:32:13 +0400

    Tweaks for corners theme

---
+++ app/Components/indexCornersHero.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Veer\Components;
+
+use Veer\Services\Show\Page as ShowPage;
+
+class indexCornersHero
+{
+    public $data;
+
+    public function __construct()
+    {
+        $id = db_parameter('HERO_ON_HOME');
+
+        if (!empty($id)) {
+            $this->data = (new ShowPage)->getPage($id, app('veer')->siteId);
+
+            $this->data->load(array('images' => function($q) {
+                return $q->orderBy('pivot_id', 'asc');
+            }));
+        }
+    }
+}
--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -1,90 +1,100 @@
-<?php namespace Veer\Http\Controllers;
+<?php
+
+namespace Veer\Http\Controllers;
 
 use Veer\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Redirect;
 use Veer\Services\Show\Page as ShowPage;
 
-class PageController extends Controller {
+class PageController extends Controller
+{
+    protected $showPage;
 
-	protected $showPage;
-	
-	public function __construct(ShowPage $showPage)
-	{
-		parent::__construct();
-		
-		$this->showPage = $showPage;
-	}
-	
-	/**
-	 * Display a listing of the resource.
-	 *
-	 */
-	public function index()
-	{
-		$pages = $this->showPage->getPagesWithSite(app('veer')->siteId);  
-				
-		$pages->load('categories', 'user');
-		
-		return $this->viewIndex('pages', $pages);
-		
-		// TODO: number of comments?
-	}
+    public function __construct(ShowPage $showPage)
+    {
+        parent::__construct();
 
-	/**
-	 * Display the specified resource.
-	 *
-	 * @param  int  $id
-	 */
-	public function show($id)
-	{		
-		// if exist external content then send it to browser, increment views
-		$external_content = $this->showPage->checkAndGetContentFromHtmlFile(app('veer')->siteId, $id);
+        $this->showPage = $showPage;
+    }
 
-        if (!empty($external_content)) 
-		{    
-			\Veer\Models\Page::where('id','=',$id)->increment('views');
+    /**
+     * Display a listing of the resource.
+     *
+     */
+    public function index()
+    {
+        $pages = $this->showPage->getPagesWithSite(app('veer')->siteId);
 
-            return \Response::make($external_content, 200)->header('Content-type','text/html');
+        $pages->load('categories', 'user');
+
+        return $this->viewIndex('pages', $pages);
+
+        // TODO: number of comments?
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     */
+    public function show($id)
+    {
+        // if exist external content then send it to browser, increment views
+        $external_content = $this->showPage->checkAndGetContentFromHtmlFile(app('veer')->siteId,
+            $id);
+
+        if (!empty($external_content)) {
+            \Veer\Models\Page::where('id', '=', $id)->increment('views');
+
+            return \Response::make($external_content, 200)->header('Content-type',
+                    'text/html');
         }
-		
-		$page = $this->showPage->getPage($id, app('veer')->siteId);                 
 
-		if(!is_object($page)) { return Redirect::route('page.index'); }
-		
-		$page->increment('views');	
+        $page = $this->showPage->getPage($id, app('veer')->siteId);
 
-		$page->load(array('images' => function($q) {
-			return $q->orderBy('pivot_id', 'asc');
-		}, 'tags', 'attributes', 'downloads', 'userlists', 'user'));
-		
-		if($page->show_comments == 1) $this->showPage->loadComments($page, 'page');
+        if (!is_object($page)) {
+            return Redirect::route('page.index');
+        }
 
-		$paginator_and_sorting = get_paginator_and_sorting();
-		
-		$data = array(
-			"page" => $page,
-			"subpages" => $this->showPage->withChildPages(app('veer')->siteId, $page->id, $paginator_and_sorting),
-			"parentpages" => $this->showPage->withParentPages(app('veer')->siteId, $page->id, $paginator_and_sorting),
-			"products" => $this->showPage->withProducts(app('veer')->siteId, $page->id, $paginator_and_sorting),
-			"categories" => $this->showPage->withCategories(app('veer')->siteId, $page->id),
-			"data" => $this->veer->loadedComponents,
-			"template" => $this->template
-		); 
-					
-		$blade_path = $this->template. '.pages.' . $id;
-				
-		$viewLink = $this->template . '.page';
-		
-		// page with special design 
-		if($page->original == 1 && \View::exists($blade_path)) $viewLink = $blade_path;
-		
-		$view = viewx($viewLink, $data);
-		
-		$this->view = $view; 
+        $page->increment('views');
 
-		return $view;			   
-	}
-	
+        $page->load(array('images' => function($q) {
+            return $q->orderBy('pivot_id', 'asc');
+        }, 'tags', 'attributes', 'downloads', 'userlists', 'user'));
+
+        if ($page->show_comments == 1)
+                $this->showPage->loadComments($page, 'page');
+
+        $paginator_and_sorting = get_paginator_and_sorting();
+
+        $data = array(
+            "page" => $page,
+            "subpages" => $this->showPage->withChildPages(app('veer')->siteId,
+                $page->id, $paginator_and_sorting),
+            "parentpages" => $this->showPage->withParentPages(app('veer')->siteId,
+                $page->id, $paginator_and_sorting),
+            "products" => $this->showPage->withProducts(app('veer')->siteId,
+                $page->id, $paginator_and_sorting),
+            "categories" => $this->showPage->withCategories(app('veer')->siteId,
+                $page->id),
+            "data" => $this->veer->loadedComponents,
+            "template" => $this->template
+        );
+
+        $blade_path = $this->template.'.pages.'.$id;
+
+        $viewLink = $this->template.'.page';
+
+        // page with special design
+        if ($page->original == 1 && \View::exists($blade_path))
+                $viewLink = $blade_path;
+
+        $view = viewx($viewLink, $data);
+
+        $this->view = $view;
+
+        return $view;
+    }
 }
 
 // TODO: показывать количество комментариев
