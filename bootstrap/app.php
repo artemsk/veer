391d44884fc9ef077f35a8588cd559cdced60338
artemskiy <garcia82@yandex.ru>
2015-12-01 03:51:13 +0300

    updates to config & bootstrap

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,7 @@
 */
 
 $app = new Illuminate\Foundation\Application(
-	realpath(__DIR__.'/../')
+    realpath(__DIR__.'/../')
 );
 
 /*
@@ -27,18 +27,18 @@ $app = new Illuminate\Foundation\Application(
 */
 
 $app->singleton(
-	'Illuminate\Contracts\Http\Kernel',
-	'Veer\Http\Kernel'
+    Illuminate\Contracts\Http\Kernel::class,
+    Veer\Http\Kernel::class
 );
 
 $app->singleton(
-	'Illuminate\Contracts\Console\Kernel',
-	'Veer\Console\Kernel'
+    Illuminate\Contracts\Console\Kernel::class,
+    Veer\Console\Kernel::class
 );
 
 $app->singleton(
-	'Illuminate\Contracts\Debug\ExceptionHandler',
-	'Veer\Exceptions\Handler'
+    Illuminate\Contracts\Debug\ExceptionHandler::class,
+    Veer\Exceptions\Handler::class
 );
 
 /*
