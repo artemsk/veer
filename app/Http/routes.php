28299e6802df8032ac3ed5903f6b17651fd3e2e2
artemskiy <garcia82@yandex.ru>
2016-03-25 01:35:00 +0300

    wrap custom routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,56 +15,60 @@
 
 /* common */
 
-get("/", array('uses' => 'IndexController@index', 'as' => 'index'));
-post("/", array('uses' => 'IndexController@index', 'as' => 'index.post'));
+Route::group(['namespace' => 'Veer\Http\Controllers'], function() {
 
-/* auth & key actions - necessary */
+    get("/", array('uses' => 'IndexController@index', 'as' => 'index'));
+    post("/", array('uses' => 'IndexController@index', 'as' => 'index.post'));
 
-get('user/logout', array('uses' => 'UserController@logout', 'as' => 'user.logout'));
-get('user/register', array('uses' => 'UserController@register', 'as' => 'user.register'));
-post('user/register', array('uses' => 'UserController@registerPost', 'as' => 'user.register.post'));
+    /* auth & key actions - necessary */
 
-get('user/{id?}', array('uses' => 'UserController@show', 'as' => 'user.show'));
+    get('user/logout', array('uses' => 'UserController@logout', 'as' => 'user.logout'));
+    get('user/register', array('uses' => 'UserController@register', 'as' => 'user.register'));
+    post('user/register', array('uses' => 'UserController@registerPost', 'as' => 'user.register.post'));
 
-get('user/cart', array('uses' => 'UserController@showCart', 'as' => 'user.cart.show'));
-post('user/cart', array('uses' => 'UserController@updateCart', 'as' => 'user.cart.update'));
-get('user/cart/add/{id?}', array('uses' => 'UserController@addToCart', 'as' => 'user.cart.add'));
-get('user/cart/remove/{cartId?}', array('uses' => 'UserController@removeFromCart', 'as' => 'user.cart.remove'));
-get('user/list/add/{type?}/{id?}', array('uses' => 'UserController@addToList', 'as' => 'user.list.add'));
-get('user/list/remove/{listId?}', array('uses' => 'UserController@removeFromList', 'as' => 'user.list.remove'));
-post('user/comment/add', array('uses' => 'UserController@addComment', 'as' => 'user.comment.add'));
-post('user/communication/add', array('uses' => 'UserController@addCommunication', 'as' => 'user.communication.add'));
+    get('user/{id?}', array('uses' => 'UserController@show', 'as' => 'user.show'));
 
-/* filter & search */
+    get('user/cart', array('uses' => 'UserController@showCart', 'as' => 'user.cart.show'));
+    post('user/cart', array('uses' => 'UserController@updateCart', 'as' => 'user.cart.update'));
+    get('user/cart/add/{id?}', array('uses' => 'UserController@addToCart', 'as' => 'user.cart.add'));
+    get('user/cart/remove/{cartId?}', array('uses' => 'UserController@removeFromCart', 'as' => 'user.cart.remove'));
+    get('user/list/add/{type?}/{id?}', array('uses' => 'UserController@addToList', 'as' => 'user.list.add'));
+    get('user/list/remove/{listId?}', array('uses' => 'UserController@removeFromList', 'as' => 'user.list.remove'));
+    post('user/comment/add', array('uses' => 'UserController@addComment', 'as' => 'user.comment.add'));
+    post('user/communication/add', array('uses' => 'UserController@addCommunication', 'as' => 'user.communication.add'));
 
-Route::resource('filter', 'FilterController', array('only' => array('index', 'show')));
-Route::resource('search', 'SearchController', array('only' => array('index', 'show', 'store')));
+    /* filter & search */
 
-/* main entities - page|articles & product */
+    Route::resource('filter', 'FilterController', array('only' => array('index', 'show')));
+    Route::resource('search', 'SearchController', array('only' => array('index', 'show', 'store')));
 
-Route::resource('product', 'ProductController', array('only' => array('index', 'show')));
-get(config('veer.page_route', 'page'), array('uses' => 'PageController@index', 'as' => 'page.index'));
-get(config('veer.page_route', 'page') .'/{id}', array('uses' => 'PageController@show', 'as' => 'page.show'));
+    /* main entities - page|articles & product */
 
-/* elements */
+    Route::resource('product', 'ProductController', array('only' => array('index', 'show')));
+    get(config('veer.page_route', 'page'), array('uses' => 'PageController@index', 'as' => 'page.index'));
+    get(config('veer.page_route', 'page') .'/{id}', array('uses' => 'PageController@show', 'as' => 'page.show'));
 
-get('attribute', array('uses' => 'AttributeController@index', 'as' => 'attribute.index'));
-get('attribute/{parentId?}/{childId?}', array('uses' => 'AttributeController@show', 'as' => 'attribute.show'));
+    /* elements */
 
-Route::resource('category', 'CategoryController', array('only' => array('index', 'show')));
-Route::resource('tag', 'TagController', array('only' => array('index', 'show')));
+    get('attribute', array('uses' => 'AttributeController@index', 'as' => 'attribute.index'));
+    get('attribute/{parentId?}/{childId?}', array('uses' => 'AttributeController@show', 'as' => 'attribute.show'));
 
-get('image/{template}/{filename}', array('uses' => 'ImageController@show', 'as' => 'image.show'));
+    Route::resource('category', 'CategoryController', array('only' => array('index', 'show')));
+    Route::resource('tag', 'TagController', array('only' => array('index', 'show')));
 
-/* e-commerce */
+    get('image/{template}/{filename}', array('uses' => 'ImageController@show', 'as' => 'image.show'));
 
-get('order/success', array('uses' => 'OrderController@success', 'as' => 'order.success'));
-Route::resource('order', 'OrderController', array('only' => array('index', 'show', 'store')));
+    /* e-commerce */
 
-/* custom */
-Route::any('custom/{params?}', ['uses' => 'IndexController@custom', 'as' => 'custom.index']);
+    get('order/success', array('uses' => 'OrderController@success', 'as' => 'order.success'));
+    Route::resource('order', 'OrderController', array('only' => array('index', 'show', 'store')));
 
-get('user/password/remind', 'RemindersController@getRemind');
-post('user/password/remind', 'RemindersController@postRemind');
-get('user/password/reset/{token?}', 'RemindersController@getReset');
-post('user/password/reset', 'RemindersController@postReset');
+    /* custom */
+    Route::any('custom/{params?}', ['uses' => 'IndexController@custom', 'as' => 'custom.index']);
+
+    get('user/password/remind', 'RemindersController@getRemind');
+    post('user/password/remind', 'RemindersController@postRemind');
+    get('user/password/reset/{token?}', 'RemindersController@getReset');
+    post('user/password/reset', 'RemindersController@postReset');
+
+});
